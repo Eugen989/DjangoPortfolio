958b7884f55e12cb4e6a390bb9392552f90f8737
VictoriaGailitis <heyleadrightnow@gmail.com>
2023-12-11 15:28:25 +0300

    First commit

--- Protfolio/mainWindow/admin.py
+++ Protfolio/mainWindow/admin.py
@@ -3,6 +3,8 @@ from .models import New
 from .models import Info
 from .models import Biography
 
+
+
 admin.site.register(New)
 admin.site.register(Info)
 admin.site.register(Biography)
--- Protfolio/mainWindow/views.py
+++ Protfolio/mainWindow/views.py
@@ -5,6 +5,7 @@ from .models import New
 from .models import Info
 from .models import Biography
 
+
 biography = {
         "name": "Евгений",
         "surname": "Круглик",
